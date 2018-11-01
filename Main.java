6930a35eb27f44998a5134147aff5dc12f790067
Ethan Lee <40707681+GreenPlane@users.noreply.github.com>
2018-10-31 22:22:37 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,42 +1,43 @@
 import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    Scanner scan = new Scanner (System.in);
-    
-    int [] array = new int[30];
-    int i = 0;
-    int valueFound = -1;
-    
-    for (i = 0; i < array.length; i++){
-      array[i] = (int)(Math.random() * (20) + 80);
-      // Enabling this line to run allowed to check that the array was filled with correct numbers.  System.out.println(array[i]); 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] array = new int[30];
+        int i = 0;
+        int valueFound = 0;
+        int userInput = 1;
+
+        for (i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * (20) + 80);
+            //System.out.print(array[i] + " "); ACTIVATING THIS LINE MAKES YOU NOT DIE WHEN TRYING TO TEST VALUES
+        }
+
+
+        System.out.println("Enter a value to search or enter, -1 to exit the search.");
+
+        userInput = scan.nextInt();
+
+        while (userInput != -1) {
+            valueFound = 0;
+            for (i = 0; i < array.length; i++) {
+                if (array[i] == userInput) {
+                    valueFound = 1;
+                }
+            }
+            if (valueFound == 1) {
+                System.out.println("Value was found.");
+                System.out.println("Enter a value to search or enter, -1 to exit the search.");
+
+                userInput = scan.nextInt();
+            }
+            else {
+                System.out.println("Value was not found.");
+                System.out.println("Enter a value to search or enter, -1 to exit the search.");
+
+                userInput = scan.nextInt();
+            }
+        }
     }
-    
-    for (i = 0; i < array.length; i++){
-       System.out.println("Enter a value to search or enter, -1 to exit the search.");
-    
-      int userInput = scan.nextInt();
-      
-      if (userInput == array[i]){
-        valueFound = 1;
-      }
-      if (valueFound == 1){
-      System.out.println("Value was found.");
-      }
-      else {
-      System.out.println("Value was not found.");
-    } 
-      System.out.println("Would you like to search for another number?  1 to continue, -1 to exit.");
-      
-      int userInput2 = scan.nextInt();
-      
-      if (userInput2 == 1){
-        i = i;
-      }
-      else if (i == -1){
-        i = 31;
-      }
-    }
-  }
 }
